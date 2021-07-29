35797bd5a27294dc443f17b24d78eebc1bac0fe3
taotao68 <gl11130306@163.com>
2021-07-29 22:32:28 +0800

    项目初始配置

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式
+import './styles/index.scss';
+
 Vue.config.productionTip = false
 
+// 创建vue实例，将router,store配置到跟组件
+// 把APP根组件渲染到#app节点上
 new Vue({
   router,
   store,
